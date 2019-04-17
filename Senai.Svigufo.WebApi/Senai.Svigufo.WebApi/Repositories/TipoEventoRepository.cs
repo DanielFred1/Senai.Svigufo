cf93864dc0fb84f622bb486357281894a1d0500f
Daniel Frederic Sena Da Silva <45867182851@info.local>
2019-04-17 08:16:27 -0300

    Repositorio

--- Senai.Svigufo.WebApi/Senai.Svigufo.WebApi/Repositories/TipoEventoRepository.cs
+++ Senai.Svigufo.WebApi/Senai.Svigufo.WebApi/Repositories/TipoEventoRepository.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Senai.Svigufo.WebApi.Interfaces;
+using Senai.Svigufo.WebApi.Domains;
+using Senai.Svigufo.WebApi.Repositories;
 
 
 namespace Senai.Svigufo.WebApi.Repositories
 {
     public class TipoEventoRepository : ITipoEventoRepository
     {
+        public List<TipoEventoDomain> Listar()
 
+        public void Alterar(TipoEventoDomain tipoEvento)
+
+        public TipoEventoDomain BuscarPorId(int id)
+
+        public void Cadastrar(TipoEventoDomain tipoEvento)
+
+        public void Deletar(int id)
     }
 }
